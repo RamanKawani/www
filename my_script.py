2c9530ba93d11ec691b63d9ca1ef07338fc7e08c
RamanKawani <RamanKhalid888@gmail.com>
2024-12-12 14:36:49 +0300

    Update and rename geopandas to  my_script.py

---
+++ my_script.py
@@ -0,0 +1,8 @@
+import geopandas as gpd
+
+# Example: Create a simple GeoDataFrame
+data = {'geometry': ['POINT (1 1)', 'POINT (2 2)', 'POINT (3 3)']}
+gdf = gpd.GeoDataFrame(data)
+
+# Print GeoDataFrame
+print(gdf)
